940603d01f1718ffaf1966a82c7703d6af2fb009
Jeremy Switzer <jeremy.switzer@gmail.com>
2012-12-05 13:43:17 -0600

    Fixing typo found in audit.

--- Formula/git-jira-flow.rb
+++ Formula/git-jira-flow.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 
 class GitJiraFlow < Formula
-  hompage 'https://github.com/LocalMed/git-jira-flow'
+  homepage 'https://github.com/LocalMed/git-jira-flow'
   url 'https://github.com/LocalMed/git-jira-flow.git', :tag => '0.0.1'
   version '0.0.1'
-  
+
   def install
     prefix.install Dir["*"]
     bin.install 'git-issue'
