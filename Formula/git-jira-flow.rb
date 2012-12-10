cd8eba6f829e53988ab03efe518346c7b3d37b5b
Jeremy Switzer <jeremy.switzer@gmail.com>
2012-12-10 15:40:08 -0600

    Added dependencies for jira-cli and getopt. Bumped version to 0.0.4. DEV-3

--- Formula/git-jira-flow.rb
+++ Formula/git-jira-flow.rb
@@ -4,12 +4,22 @@ require 'formula'
 class GitJiraFlow < Formula
   homepage 'https://github.com/LocalMed/git-jira-flow'
   url 'https://github.com/LocalMed/git-jira-flow.git'
-  version '0.0.3'
+  version '0.0.4'
 
   head 'https://github.com/LocalMed/git-jira-flow.git', :branch => 'master'
+
+  depends_on 'gnu-getopt'
+  depends_on 'jira-cli'
 
   def install
     bin.install "git-issue"
     prefix.install Dir['*']
+  end
+
+  def caveats; <<-EOS.undent
+    From the command prompt, run:
+        git issue start <JIRA-ID> [-b <DESCRIPTION>]
+        git issue finish [JIRA-ID]
+    EOS
   end
 end
