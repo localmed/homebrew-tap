0136ebf47106c07a0d855f8f1fd26149a8f616f8
Pete Browne <me@petebrowne.com>
2013-01-21 10:17:26 -0600

    Version bump for git-jira-flow to 0.2.0

--- Formula/git-jira-flow.rb
+++ Formula/git-jira-flow.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GitJiraFlow < Formula
   homepage 'https://github.com/localmed/git-jira-flow'
-  url 'https://github.com/localmed/git-jira-flow.git', :tag => 'v0.1.1'
-  version '0.1.1'
+  url 'https://github.com/localmed/git-jira-flow.git', :tag => 'v0.2.0'
+  version '0.2.0'
 
   head 'https://github.com/localmed/git-jira-flow.git', :branch => 'master'
 
