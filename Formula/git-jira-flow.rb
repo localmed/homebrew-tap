84324870238b2629a45c481b8a91fdd9bd4d503e
Pete Browne <me@petebrowne.com>
2013-02-18 15:00:47 -0600

    Version bump for git-jira-flow to 0.3.0

--- Formula/git-jira-flow.rb
+++ Formula/git-jira-flow.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GitJiraFlow < Formula
   homepage 'https://github.com/localmed/git-jira-flow'
-  url 'https://github.com/localmed/git-jira-flow.git', :tag => 'v0.2.1'
-  version '0.2.1'
+  url 'https://github.com/localmed/git-jira-flow.git', :tag => 'v0.3.0'
+  version '0.3.0'
 
   head 'https://github.com/localmed/git-jira-flow.git', :branch => 'master'
 
@@ -11,13 +11,13 @@ class GitJiraFlow < Formula
   depends_on 'jira-cli'
 
   def install
-    system "make", "prefix=#{prefix}", "install"
+    system 'make', "prefix=#{prefix}", 'install'
   end
 
   def caveats; <<-EOS.undent
     From the command prompt, run:
-        git issue start <ISSUE_KEY> [-b <BRANCH_NAME>]
-        git issue finish [ISSUE_KEY]
+        git issue start [<issuekey>] [-b <branchname>] [-H]
+        git issue finish [<issuekey>] [-H]
     EOS
   end
 end
