f2d431625227cb7af6a94ab197cf93485c666fd7
Jeremy Switzer <jeremy.switzer@gmail.com>
2013-02-21 12:11:55 -0600

    Bumped git-jira-flow version to 3.1

--- Formula/git-jira-flow.rb
+++ Formula/git-jira-flow.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GitJiraFlow < Formula
   homepage 'https://github.com/localmed/git-jira-flow'
-  url 'https://github.com/localmed/git-jira-flow.git', :tag => 'v0.3.0'
-  version '0.3.0'
+  url 'https://github.com/localmed/git-jira-flow.git', :tag => 'v0.3.1'
+  version '0.3.1'
 
   head 'https://github.com/localmed/git-jira-flow.git', :branch => 'master'
 
