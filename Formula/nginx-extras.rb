04473fa4f3adc6027917dde745f4222d78eb0385
Jeremy Switzer <jeremy.switzer@localmed.com>
2013-06-17 15:18:34 -0500

    Added nginx-extras formula.

---
+++ Formula/nginx-extras.rb
@@ -0,0 +1,163 @@
+require 'formula'
+
+class NginxExtras < Formula
+  homepage 'http://nginx.org/'
+  url 'https://s3.amazonaws.com/heroku-nginx-extras/nginx-extras-1.4.1.tar.gz'
+  sha1 '99987b85e9a8c9dc3688a45ecaa7b2d41eacd52a'
+  version '0.1.1'
+
+  env :userpaths
+
+  depends_on 'pcre'
+  depends_on 'openssl'
+
+  skip_clean 'logs'
+
+  # Changes default port to 8080
+  def patches
+    DATA
+  end
+
+  def config_args
+    module_path = "contrib"
+    modules = ["ngx_http_auth_request_module-a29d74804ff1"]
+
+    cc_opt = "-I#{HOMEBREW_PREFIX}/include"
+    ld_opt = "-L#{HOMEBREW_PREFIX}/lib"
+
+    openssl_path = Formula.factory("openssl").opt_prefix
+    cc_opt += " -I#{openssl_path}/include"
+    ld_opt += " -L#{openssl_path}/lib"
+
+    args = ["--prefix=#{prefix}",
+            "--with-http_ssl_module",
+            "--with-pcre",
+            "--with-ipv6",
+            "--sbin-path=#{bin}/nginx",
+            "--with-cc-opt=#{cc_opt}",
+            "--with-ld-opt=#{ld_opt}",
+            "--conf-path=#{etc}/nginx/nginx.conf",
+            "--pid-path=#{var}/run/nginx.pid",
+            "--lock-path=#{var}/run/nginx.lock",
+            "--http-client-body-temp-path=#{var}/run/nginx/client_body_temp",
+            "--http-proxy-temp-path=#{var}/run/nginx/proxy_temp",
+            "--http-fastcgi-temp-path=#{var}/run/nginx/fastcgi_temp",
+            "--http-uwsgi-temp-path=#{var}/run/nginx/uwsgi_temp",
+            "--http-scgi-temp-path=#{var}/run/nginx/scgi_temp",
+            "--http-log-path=#{var}/log/nginx",
+            "--with-http_gzip_static_module",
+            "--with-http_gunzip_module",
+            "--with-http_spdy_module",
+            "--with-debug",
+            "--with-http_dav_module",
+            "--with-http_addition_module",
+            "--with-http_degradation_module",
+            "--with-http_flv_module",
+            "--with-http_mp4_module",
+            "--with-http_random_index_module",
+            "--with-http_realip_module",
+            "--with-http_secure_link_module",
+            "--with-http_stub_status_module",
+            "--with-http_sub_module",
+            "--with-http_xslt_module",
+            "--add-module=#{module_path}/#{modules[0]}"
+          ]
+    return args
+
+
+    puts "Unable to install nginx with passenger support. The passenger"
+    puts "gem must be installed and passenger-config must be in your path"
+    puts "in order to continue."
+    exit
+  end
+
+  def install
+
+    args = config_args
+
+    if build.head?
+      system "./auto/configure", *args
+    else
+      system "./configure", *args
+    end
+    system "make"
+    system "make install"
+    man8.install "objs/nginx.8"
+    (var/'run/nginx').mkpath
+
+    # nginx’s docroot is #{prefix}/html, this isn't useful, so we symlink it
+    # to #{HOMEBREW_PREFIX}/var/www. The reason we symlink instead of patching
+    # is so the user can redirect it easily to something else if they choose.
+    prefix.cd do
+      dst = HOMEBREW_PREFIX/"var/www"
+      if not dst.exist?
+        dst.dirname.mkpath
+        mv "html", dst
+      else
+        rm_rf "html"
+        dst.mkpath
+      end
+      Pathname.new("#{prefix}/html").make_relative_symlink(dst)
+    end
+
+    # for most of this formula’s life the binary has been placed in sbin
+    # and Homebrew used to suggest the user copy the plist for nginx to their
+    # ~/Library/LaunchAgents directory. So we need to have a symlink there
+    # for such cases
+    if (HOMEBREW_CELLAR/'nginx-extras').subdirs.any?{|d| (d/:sbin).directory? }
+      sbin.mkpath
+      sbin.cd do
+        (sbin/'nginx').make_relative_symlink(bin/'nginx')
+      end
+    end
+  end
+
+  def caveats; <<-EOS.undent
+    Docroot is: #{HOMEBREW_PREFIX}/var/www
+
+    The default port has been set to 8080 so that nginx can run without sudo.
+
+    If you want to host pages on your local machine to the wider network you
+    can change the port to 80 in: #{HOMEBREW_PREFIX}/etc/nginx/nginx.conf
+
+    You will then need to run nginx as root: `sudo nginx`.
+    EOS
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <false/>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_prefix}/bin/nginx</string>
+            <string>-g</string>
+            <string>daemon off;</string>
+        </array>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+      </dict>
+    </plist>
+    EOS
+  end
+end
+
+__END__
+--- a/conf/nginx.conf
++++ b/conf/nginx.conf
+@@ -33,7 +33,7 @@
+     #gzip  on;
+
+     server {
+-        listen       80;
++        listen       8080;
+         server_name  localhost;
+
+         #charset koi8-r;
