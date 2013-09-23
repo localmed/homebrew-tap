1274c9e6b3669c8399a25927995eb9623a1649ae
Chris Keele <dev@chriskeele.com>
2013-09-23 10:54:41 -0500

    Update git-jira-issue-flow.

--- Formula/git-jira-flow.rb
+++ Formula/git-jira-flow.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GitJiraFlow < Formula
   homepage 'https://github.com/localmed/git-jira-flow'
-  url 'https://github.com/localmed/git-jira-flow.git', :tag => 'v0.3.3'
-  version '0.3.3'
+  url 'https://github.com/localmed/git-jira-flow.git', :tag => 'v0.3.5'
+  version '0.3.5'
 
   head 'https://github.com/localmed/git-jira-flow.git', :branch => 'master'
 
